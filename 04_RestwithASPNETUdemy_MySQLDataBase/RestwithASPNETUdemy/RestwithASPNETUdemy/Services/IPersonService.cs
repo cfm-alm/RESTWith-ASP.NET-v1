4a9e9afc6056db797160de7cc6b959f1bb6ab1ed
cfm-alm <ceciliafossamalmeida@usp.br>
2024-01-13 16:00:17 -0300

    Integrating API with SQL Database

---
+++ 04_RestwithASPNETUdemy_MySQLDataBase/RestwithASPNETUdemy/RestwithASPNETUdemy/Services/IPersonService.cs
@@ -0,0 +1,13 @@
+﻿using RestwithASPNETUdemy.Model;
+
+namespace RestwithASPNETUdemy.Services
+{
+    public interface IPersonService
+    {
+        Person Create(Person person);
+        Person FindById(long id);
+        List<Person> FindAll();
+        Person Update(Person person);
+        void Delete(long id);
+    }
+}
