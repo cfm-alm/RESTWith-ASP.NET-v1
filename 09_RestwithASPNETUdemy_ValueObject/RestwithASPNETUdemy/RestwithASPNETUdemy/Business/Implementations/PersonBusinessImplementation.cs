32abfa5535e94f6b681bcc223ff420aa9151ab41
cfm-alm <ceciliafossamalmeida@usp.br>
2024-01-13 22:32:23 -0300

    Working with Value Objects

---
+++ 09_RestwithASPNETUdemy_ValueObject/RestwithASPNETUdemy/RestwithASPNETUdemy/Business/Implementations/PersonBusinessImplementation.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Query.SqlExpressions;
+using RestwithASPNETUdemy.Data.Converter.Implementations;
+using RestwithASPNETUdemy.Data.VO;
+using RestwithASPNETUdemy.Model;
+using RestwithASPNETUdemy.Model.Context;
+using RestwithASPNETUdemy.Repository;
+using System;
+
+namespace RestwithASPNETUdemy.Business.Implementations
+{
+    public class PersonBusinessImplementation : IPersonBusiness
+    {
+        private readonly IRepository<Person> _repository;
+
+        private readonly PersonConverter _converter;
+
+        public PersonBusinessImplementation(IRepository<Person> repository)
+        {
+            _repository = repository;
+            _converter = new PersonConverter();
+        }
+
+        public List<PersonVO> FindAll()
+        {
+            return _converter.Parse(_repository.FindAll());
+        }
+
+        public PersonVO FindById(long id)
+        {
+            return _converter.Parse(_repository.FindById(id));
+        }
+        public PersonVO Create(PersonVO person)
+        {
+            var personEntity = _converter.Parse(person);
+            personEntity = _repository.Create(personEntity);
+            return _converter.Parse(personEntity);
+        }
+        public PersonVO Update(PersonVO person)
+        {
+            var personEntity = _converter.Parse(person);
+            personEntity = _repository.Update(personEntity);
+            return _converter.Parse(personEntity);
+        }
+
+        public void Delete(long id)
+        {
+            _repository.Delete(id);
+        }
+
+    }
+}
