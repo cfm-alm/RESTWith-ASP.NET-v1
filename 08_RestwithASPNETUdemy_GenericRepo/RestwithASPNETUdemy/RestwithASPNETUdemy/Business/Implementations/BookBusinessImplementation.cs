f6cfb0978d3ac4b522661f6211cdabb01571131a
cfm-alm <ceciliafossamalmeida@usp.br>
2024-01-13 21:52:02 -0300

    Adding Generic Repository

---
+++ 08_RestwithASPNETUdemy_GenericRepo/RestwithASPNETUdemy/RestwithASPNETUdemy/Business/Implementations/BookBusinessImplementation.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Query.SqlExpressions;
+using RestwithASPNETUdemy.Model;
+using RestwithASPNETUdemy.Model.Context;
+using RestwithASPNETUdemy.Repository;
+using System;
+
+namespace RestwithASPNETUdemy.Business.Implementations
+{
+    public class BookBusinessImplementation : IBookBusiness
+    {
+        private readonly IRepository<Book> _repository;
+
+        public BookBusinessImplementation(IRepository<Book> repository)
+        {
+            _repository = repository;
+        }
+
+        public List<Book> FindAll()
+        {
+            return _repository.FindAll();
+        }
+
+        public Book FindById(long id)
+        {
+            return _repository.FindById(id);
+        }
+        public Book Create(Book book)
+        {
+            return _repository.Create(book);
+        }
+        public Book Update(Book book)
+        {
+            return _repository.Update(book);
+        }
+
+        public void Delete(long id)
+        {
+            _repository.Delete(id);
+        }
+
+    }
+}
---
+++ 08_RestwithASPNETUdemy_GenericRepo/RestwithASPNETUdemy/RestwithASPNETUdemy/Business/Implementations/PersonBusinessImplementation.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Query.SqlExpressions;
+using RestwithASPNETUdemy.Model;
+using RestwithASPNETUdemy.Model.Context;
+using RestwithASPNETUdemy.Repository;
+using System;
+
+namespace RestwithASPNETUdemy.Business.Implementations
+{
+    public class PersonBusinessImplementation : IPersonBusiness
+    {
+        private readonly IRepository<Person> _repository;
+
+        public PersonBusinessImplementation(IRepository<Person> repository)
+        {
+            _repository = repository;
+        }
+
+        public List<Person> FindAll()
+        {
+            return _repository.FindAll();
+        }
+
+        public Person FindById(long id)
+        {
+            return _repository.FindById(id);
+        }
+        public Person Create(Person person)
+        {
+            return _repository.Create(person);
+        }
+        public Person Update(Person person)
+        {
+            return _repository.Update(person);
+        }
+
+        public void Delete(long id)
+        {
+            _repository.Delete(id);
+        }
+
+    }
+}
