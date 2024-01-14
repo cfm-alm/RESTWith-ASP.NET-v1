e7ea30d0dc9b6cb3cb7bc01cb90ce8f27cb1ed81
cfm-alm <ceciliafossamalmeida@usp.br>
2024-01-14 15:51:20 -0300

    Content Negotiation - Adding Support to .xml and .json

---
+++ 11_RestwithASPNETUdemy_ContentNegotiation/RestwithASPNETUdemy/RestwithASPNETUdemy/Controllers/BookController.cs
@@ -0,0 +1,59 @@
+using Microsoft.AspNetCore.Mvc;
+using RestwithASPNETUdemy.Model;
+using RestwithASPNETUdemy.Business;
+using RestwithASPNETUdemy.Data.VO;
+
+namespace RestWithASPNETErudio.Controllers
+{
+    [ApiVersion("1")]
+    [ApiController]
+    [Route("api/[controller]/v{version:apiVersion}")]
+    public class BookController : ControllerBase
+    {
+
+        private readonly ILogger<BookController> _logger;
+        private IBookBusiness _bookBusiness;
+        public BookController(ILogger<BookController> logger, IBookBusiness bookBusiness)
+        {
+            _logger = logger;
+            _bookBusiness = bookBusiness;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_bookBusiness.FindAll());
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(long id)
+        {
+            var book = _bookBusiness.FindById(id);
+            if (book == null) return NotFound();
+            return Ok(book);
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] BookVO book)
+        {
+            if (book == null) return BadRequest();
+            return Ok(_bookBusiness.Create(book));
+        }
+
+        [HttpPut]
+        public IActionResult Put([FromBody] BookVO book)
+        {
+            if (book == null) return BadRequest();
+            return Ok(_bookBusiness.Update(book));
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(long id)
+        {
+            _bookBusiness.Delete(id);
+            return NoContent();
+        }
+
+
+    }
+}
---
+++ 11_RestwithASPNETUdemy_ContentNegotiation/RestwithASPNETUdemy/RestwithASPNETUdemy/Data/Converter/Implementations/BookConverter.cs
@@ -0,0 +1,47 @@
+﻿using RestwithASPNETUdemy.Data.Converter.Contract;
+using RestwithASPNETUdemy.Data.VO;
+using RestwithASPNETUdemy.Model;
+
+namespace RestwithASPNETUdemy.Data.Converter.Implementations
+{
+    public class BookConverter : IParser<BookVO, Book>, IParser<Book, BookVO>
+    {
+        public Book Parse(BookVO origem)
+        {
+            if (origem == null) return null;
+            return new Book
+            {
+                Id = origem.Id,
+                Author = origem.Author,
+                LaunchDate = origem.LaunchDate,
+                Price = origem.Price,
+                Title = origem.Title
+            };
+        }
+
+        public BookVO Parse(Book origem)
+        {
+            if (origem == null) return null;
+            return new BookVO
+            {
+                Id = origem.Id,
+                Author = origem.Author,
+                LaunchDate = origem.LaunchDate,
+                Price = origem.Price,
+                Title = origem.Title
+            };
+        }
+
+        public List<BookVO> Parse(List<Book> origem)
+        {
+            if (origem == null) return null;
+            return origem.Select(item => Parse(item)).ToList();
+        }
+
+        public List<Book> Parse(List<BookVO> origem)
+        {
+            if (origem == null) return null;
+            return origem.Select(item => Parse(item)).ToList();
+        }
+    }
+}
