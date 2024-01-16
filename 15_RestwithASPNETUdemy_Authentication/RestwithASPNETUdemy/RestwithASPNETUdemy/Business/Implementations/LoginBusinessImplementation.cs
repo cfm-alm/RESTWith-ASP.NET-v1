f826a5704c24248409a97e4a761090f88d427316
cfm-alm <ceciliafossamalmeida@usp.br>
2024-01-16 16:28:19 -0300

    Adding Authentication Support

---
+++ 15_RestwithASPNETUdemy_Authentication/RestwithASPNETUdemy/RestwithASPNETUdemy/Business/Implementations/LoginBusinessImplementation.cs
@@ -0,0 +1,100 @@
+﻿using RestwithASPNETUdemy.Configurations;
+using RestwithASPNETUdemy.Data.VO;
+using RestwithASPNETUdemy.Model;
+using RestwithASPNETUdemy.Repository;
+using RestwithASPNETUdemy.Services;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace RestwithASPNETUdemy.Business.Implementations
+{
+    public class LoginBusinessImplementation : ILoginBusiness
+    {
+        private const string DATE_FORMAT = "yyyy-MM-dd HH:mm:ss";
+        private TokenConfiguration _configuration;
+
+        private IUserRepository _repository;
+        private readonly ITokenService _tokenService;
+
+        public LoginBusinessImplementation(TokenConfiguration configuration, IUserRepository repository, ITokenService tokenService)
+        {
+            _configuration = configuration;
+            _repository = repository;
+            _tokenService = tokenService;
+        }
+
+        public TokenVO ValidateCredentials(UserVO userCredentials)
+        {
+            var user = _repository.ValidateCredentials(userCredentials);
+            if (user == null) return null;
+            var claims = new List<Claim>
+            {
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString("N")),
+                new Claim(JwtRegisteredClaimNames.UniqueName, user.UserName)
+            };
+
+            var accessToken = _tokenService.GenerateAccessToken(claims);
+            var refreshToken = _tokenService.GenerateRefreshToken();
+
+            user.RefreshToken = refreshToken;
+            user.RefreshTokenExpiryTime = DateTime.Now.AddDays(_configuration.DaysToExpiry);
+
+            _repository.RefreshUserInfo(user);
+
+            DateTime createDate = DateTime.Now;
+            DateTime expirationDate = createDate.AddMinutes(_configuration.Minutes);
+
+            return new TokenVO(
+                true,
+                createDate.ToString(DATE_FORMAT),
+                expirationDate.ToString(DATE_FORMAT),
+                accessToken,
+                refreshToken
+                );
+        }
+
+        public TokenVO ValidateCredentials(TokenVO token)
+        {
+            var accessToken = token.AccessToken;
+            var refreshToken = token.RefreshToken;
+
+            var principal = _tokenService.GetPrincipalFromExpiredToken(accessToken);
+
+            var username = principal.Identity.Name;
+
+            var user = _repository.ValidateCredentials(username);
+
+            if (user == null ||
+                user.RefreshToken != refreshToken ||
+                user.RefreshTokenExpiryTime <= DateTime.Now) return null;
+
+            accessToken = _tokenService.GenerateAccessToken(principal.Claims);
+            refreshToken = _tokenService.GenerateRefreshToken();
+
+            user.RefreshToken = refreshToken;
+
+            _repository.RefreshUserInfo(user);
+
+            DateTime createDate = DateTime.Now;
+            DateTime expirationDate = createDate.AddMinutes(_configuration.Minutes);
+
+            return new TokenVO(
+                true,
+                createDate.ToString(DATE_FORMAT),
+                expirationDate.ToString(DATE_FORMAT),
+                accessToken,
+                refreshToken
+                );
+        }
+
+        public bool RevokeToken(string userName)
+        {
+            return _repository.RevokeToken(userName);
+        }
+    }
+    // pega as credenciais e valida no banco, se etsiver correto, retorna o user, se user == null, retorna nulo e faz a validação no controller
+    // caso contrário, gera as claims, accessToken, refreshToken e seta esses valores no usuário, define a data de criação do token (agora) e de expiração (agora + mins)
+    //atualiza as informações do user
+    // seta as informações do token (autenticado = true, createDate, expirationDate, access e refresh tokens
+
+}
